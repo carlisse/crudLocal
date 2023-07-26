71086516f248e991147930aabbaadc71460b25a1
elizabeth_sanchez <eliz9016@hotmail.es>
2023-07-26 13:29:30 -0700

    add

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 //form
 function validateForm(){
-    let email = document.getElementById('inputEmail').value;
     let name = document.getElementById('inputName').value;
+    let email = document.getElementById('inputEmail').value;
     let phone = document.getElementById('inputPhone').value;
+    
+    if (name == "") {
+        alert('El Nombre es requerido');
+        return false;
+    
+    }
 
    if (email == "") {
         alert('El correo es requerido');
         return false;
     }   else if(!email.includes('@')){
         alert('El correo no es valido');
-    }
-
-   if (name == "") {
-        alert('El Nombre es requerido');
-        return false;
-    
     }
 
     if (phone == "") {
@@ -41,8 +41,8 @@ function readData() {
 
     listPeople.forEach(function(element, index) {
         html += "<tr>";
-        html += "<td>"+ element.email + "</td>";
         html += "<td>"+ element.name + "</td>";
+        html += "<td>"+ element.email + "</td>";
         html += "<td>"+ element.phone + "</td>";
         html += '<td><button onclick="deleData('+ index +')" class="btn btn-danger">Eliminar</button><button onclick="editData('+ index +')" class="btn btn-warning">Editar</button></td>';
         html += "</tr>";
@@ -55,8 +55,8 @@ document.onload = readData();
 
 function AddData() {
     if (validateForm() == true) {
-        let email = document.getElementById('inputEmail').value;
         let name = document.getElementById('inputName').value;
+        let email = document.getElementById('inputEmail').value;
         let phone = document.getElementById('inputPhone').value;
         
         var listPeople;
@@ -68,8 +68,8 @@ function AddData() {
         }
 
         listPeople.push({
-            email: email,
             name: name,
+            email: email,
             phone: phone
         });
 
@@ -77,8 +77,8 @@ function AddData() {
 
         readData();
 
-        document.getElementById('inputEmail').value= "";
         document.getElementById('inputName').value= "";
+        document.getElementById('inputEmail').value= "";
         document.getElementById('inputPhone').value= "";
     }
 }
@@ -111,21 +111,21 @@ function editData(index) {
         listPeople = JSON.parse(localStorage.getItem('listPeople'));
     }
 
-    document.getElementById('inputEmail').value = listPeople[index].email;
     document.getElementById('inputName').value = listPeople[index].name;
+    document.getElementById('inputEmail').value = listPeople[index].email;
     document.getElementById('inputPhone').value = listPeople[index].phone;
 
     document.querySelector('#btnUpdate').onclick = function () {
         if (validateForm() == true) {
-            listPeople[index].email = document.getElementById('inputEmail').value;
             listPeople[index].name = document.getElementById('inputName').value;
+            listPeople[index].email = document.getElementById('inputEmail').value;
             listPeople[index].phone = document.getElementById('inputPhone').value;
 
             localStorage.setItem('listPeople', JSON.stringify(listPeople));
             readData();
 
-            document.getElementById('inputEmail').value = "";
             document.getElementById('inputName').value = "";
+            document.getElementById('inputEmail').value = "";
             document.getElementById('inputPhone').value = "";
 
             document.getElementById('btnAdd').style.display = 'block';
